154458f6fc29da650cc674134da9167bf05372f7
wangxiaobo33 <wangxiaobo9@jd.com>
2020-04-16 10:13:31 +0800

    click demo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import {Route,Switch} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {count:0}
+  this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(){
+    this.setState({
+      count:this.state.count+1
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -19,6 +31,8 @@ class App extends Component {
           >
             Learn React
           </a>
+          <div>{this.state.count}</div>
+          <button onClick={this.handleClick}>update</button>
         </header>
       </div>
     );
